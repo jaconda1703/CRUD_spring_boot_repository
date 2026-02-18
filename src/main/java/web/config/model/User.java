1a0cd765365caa9f6724ca1a03f032120025807e
Jaconda <jaconda007@rambler.ru>
2026-02-18 03:38:42 +0300

    Add full project

--- src/main/java/web/config/model/User.java
+++ src/main/java/web/config/model/User.java
@@ -1,4 +1,28 @@
 package web.config.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "age")
+    private int age;
 }
